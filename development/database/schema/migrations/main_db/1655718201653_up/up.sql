a07d262277bc554c5fc74a05759007a423aa6f48
Zitao Xiong <caoer115@gmail.com>
2024-05-23 18:10:25 +0800

    add dev

---
+++ development/database/schema/migrations/main_db/1655718201653_up/up.sql
@@ -0,0 +1,17 @@
+CREATE
+  EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE
+  OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+  RETURNS TRIGGER AS
+$$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$
+  LANGUAGE plpgsql;
+
